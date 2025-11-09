13c56d3da5602e93afaede302957a09378b000a4
CharlieC <85197472+PHCEAC@users.noreply.github.com>
2025-11-09 22:03:34 +0100

    find which pool is full
    Add script to find full storage pools in Qubes

---
+++ dom0-find-full-storage-pool.sh
@@ -0,0 +1,6 @@
+# Qubes specific
+# list the logical volumes named xxxxxx-pool
+sudo lvs | head -n 1 ; sudo lvs | grep -e "^\s*[^ ]*pool "
+
+# Notes:
+## Could probably also use the Attributes column...
